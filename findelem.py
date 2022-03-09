747d5788192e4e67cf673301a4bb23a1d2198bd2
Denizcan <denizcan.ozpinar@ozu.edu.tr>
2022-03-09 21:12:46 +0300

    Added findelem py

---
+++ findelem.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+driver = webdriver.Chrome("/Users/denis/Downloads/chromedriver")
+driver.get("http://localhost:8080/owners/find")
+driver.maximize_window()
+
+a_elements = driver.find_elements(By.TAG_NAME,"a")
+button_elements = driver.find_elements(By.CLASS_NAME,"btn btn-primary")
+
+for elem in a_elements:
+
+    print(elem.get_attribute("href"))
+    if elem.accessible_name.__contains__("FIND OWNERS"):
+        ftmp =elem.get_property("class")
+print(button_elements)
+
+
+
+for elem in button_elements:
+    elem.click()
+
+time.sleep(5)
+driver.quit()

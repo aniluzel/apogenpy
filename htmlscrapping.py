86924b89e47f5398b9f1a0bc516b8d092b5af7ed
ozgunkarahan <ozgun.karahan@ozu.edu.tr>
2022-04-09 20:24:57 +0300

    small tests for selenium WIP

--- htmlscrapping.py
+++ htmlscrapping.py
@@ -1,15 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-#2 libraries to get bs4 and requests to get html from webpage
-url = "http://localhost:8080"
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+service = Service(r'C:\chromedriver.exe')
+browser = webdriver.Chrome(service=service)
+
+
+browser.get("http://localhost:8080/owners/find")
+
+url = "http://localhost:8080/owners/find"
 
 page_html = requests.get(url).text
 
 soup = BeautifulSoup(page_html, "html.parser")
 title =soup.title
+#titles = soup.find_all('title')
+#buttons = soup.find_all('button')
+button2= soup.find_all(class_="btn btn-primary")
+#print(titles)
 
-buttons = soup.find_all('button')
+print(button2)
+
+print("----------------------------")
+AddOwner=browser.find_element(By.CSS_SELECTOR, ".btn-primary")
+#time.sleep(3)
+AddOwner.click()
 items = soup.find_all('item')
 print('title: '+title.string)
 div_tags = soup.find_all('div')
@@ -19,7 +39,6 @@ for div in div_tags:
      ID = div.get('id')
      if ID is not None:
          ids.append(ID)
-
 
 
 classes_= soup.find_all(class_="")
@@ -39,7 +58,6 @@ jsonAppender(ids,'scrappedData.json')
 refs = soup.find_all('a',href=True)
 for i in refs:
     jsonAppender(i.text, 'scrappedHrefData.json')
-
-print(refs)
+#print(refs)
 
 
